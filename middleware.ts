b4043197203320e43a3460b9d2cc79c8e6204215
damnjoejoe <kevinyuqi123@gmail.com>
2023-11-05 23:30:32 -0600

    detect env and set up session

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,16 @@ import { NextResponse } from "next/server";
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const cookies = request.cookies.getAll();
-  const session = request.cookies.get("next-auth.session-token");
+  let session = request.cookies.get("next-auth.session-token");
+  if (process.env.NODE_ENV === "production") {
+    session = request.cookies.get("__Secure-next-auth.callback-url");
+  }
   console.log("MIDDLEWARE", {
     cookies,
     session,
     url: request.url,
     nextUrl: request.nextUrl,
+    env: process.env.NODE_ENV,
   });
   if (!session)
     return NextResponse.redirect(
